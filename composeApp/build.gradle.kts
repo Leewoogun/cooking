4abf7dfc708e7aa61f301e957610e20816d1effa
Leewoogun98 <wglee@logishm.com>
2025-12-21 12:46:53 +0900

    fix: 버전 카탈로그 의존성 수정

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -37,8 +37,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodelCompose)
-            implementation(libs.androidx.lifecycle.runtimeCompose)
+
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
